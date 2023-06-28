b9e7ded8c30c7e358f805aa5fd1157220b0cdc8f
MoDurick <137512289+MoDurick@users.noreply.github.com>
2023-06-29 05:02:48 +0700

    Refactor variable names

---
+++ yellow_king_mountain.lua
@@ -0,0 +1,115 @@
+-- The Mindful Entrepreneur --
+ 
+-- Chapter 1: Introduction --
+ 
+-- Setting the Scene --
+ 
+-- We start with a basic introduction to the concept of being a mindful entrepreneur.
+ 
+-- Declare Variables
+ 
+local mindfulEntrepreneur = "A mindful entrepreneur is a person who takes the time to be aware of their thoughts, feelings, and actions while pursuing their business goals."
+local benefitsOfMindfulness = "The benefits of mindfulness for entrepreneurs include improved decision-making ability, increased motivation, clarity of thought, and improved relationships with colleagues."
+
+-- Create a Table to Store Data
+ 
+local mindfulnessData = {
+    introduction = mindfulEntrepreneur,
+    benefits = benefitsOfMindfulness
+}
+
+-- Create Function to Print Introduction
+ 
+function printIntroduction()
+    print(mindfulnessData["introduction"])
+end
+
+-- Create Function to Print Benefits
+ 
+function printBenefits()
+    print(mindfulnessData["benefits"])
+end
+
+-- Chapter 2: Mindful Strategies --
+ 
+-- Using Mindfulness in Business Decisions --
+ 
+-- Let's look at how entrepreneurs can use mindfulness to make better business decisions.
+ 
+-- Declare Variables
+ 
+local mindfulnessInBusiness = "It is important to be aware of our emotions and reactions when making business decisions, as this can help us make more informed and thought-out choices."
+local mindfulStrategies = "Mindful strategies can include taking a moment to reflect on the potential consequences of our decisions before moving forward, or taking time to analyze our feelings to gain more insight into our motivations."
+
+-- Update Table to Store Data
+ 
+mindfulnessData["business"] = mindfulnessInBusiness
+mindfulnessData["strategies"] = mindfulStrategies
+
+-- Create Function to Print Strategies
+ 
+function printStrategies()
+    print(mindfulnessData["strategies"])
+end
+
+-- Create Function to Print Mindfulness in Business
+ 
+function printMindfulnessInBusiness()
+    print(mindfulnessData["business"])
+end
+
+-- Chapter 3: Mindful Entrepreneurship --
+ 
+-- A Mindful Approach to Entrepreneurship --
+ 
+-- Let's explore how entrepreneurs can use mindfulness to succeed.
+ 
+-- Declare Variables
+ 
+local mindfulApproach = "A mindful approach to entrepreneurship involves creating a plan, setting achievable goals, and staying focused on the present rather than worrying about the future."
+local mindfulHabits = "Successful entrepreneurs can benefit from forming positive, mindful habits such as mindfulness meditation, journaling, and taking time for self-care."
+
+-- Update Table to Store Data
+ 
+mindfulnessData["approach"] = mindfulApproach
+mindfulnessData["habits"] = mindfulHabits
+
+-- Create Function to Print Approach
+ 
+function printApproach()
+    print(mindfulnessData["approach"])
+end
+
+-- Create Function to Print Habits
+ 
+function printHabits()
+    print(mindfulnessData["habits"])
+end
+
+-- Chapter 4: Conclusion --
+ 
+-- Mindful Entrepreneurship as a Whole --
+ 
+-- To recap, mindfulness is an important part of being a successful entrepreneur.
+ 
+-- Declare Variables
+ 
+local mindfulConclusion = "Mindfulness helps entrepreneurs focus on the present and make more informed, thoughtful decisions. It also encourages positive habits such as meditation, journaling, and self-care that can help entrepreneurs stay motivated and productive."
+local mindfulSummary = "In summary, mindful entrepreneurs are aware of their thoughts, feelings, and actions while striving to achieve their business goals."
+
+-- Update Table to Store Data
+ 
+mindfulnessData["conclusion"] = mindfulConclusion
+mindfulnessData["summary"] = mindfulSummary
+
+-- Create Function to Print Summary
+ 
+function printSummary()
+    print(mindfulnessData["summary"])
+end
+
+-- Create Function to Print Conclusion
+ 
+function printConclusion()
+    print(mindfulnessData["conclusion"])
+end
